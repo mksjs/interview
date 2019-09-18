2f699884e03d25e3ee172fdbec869daf4df15777
manish kumar <mksjs1998@gmail.com>
2019-09-18 17:08:38 +0530

    Two string is given check wheather both are permutation or not -Verison 1

--- checkPerm.java
+++ checkPerm.java
@@ -1,6 +1,23 @@
 import java.util.*;
 
 class checkPerm{
+	static boolean permutationVer1(String s,String t){
+		if(s.length()!=t.length()){
+			return false;
+		}
+		int [] letter = new int[128]; //assumption
+		char[] s_array = s.toCharArray();
+		for(char ch : s_array){
+			letter[ch]++;
+		}
+		for(int i=0;i<t.length();i++){
+			int c = (int)t.charAt(i);
+			letter[c]--;
+			if(letter[c] <0)
+				return false;
+		}
+		return true;
+	}
 	static String sort(String str){
 		char[] content = str.toCharArray();
 		Arrays.sort(content);
@@ -22,6 +39,12 @@ class checkPerm{
 		else{
 			System.out.println("not permutation of each other");
 		}
-		
+		System.out.println("Version 1");
+		if(permutationVer1(s,t)){
+                        System.out.println("one String is permutaion of other");
+                }
+                else{
+                        System.out.println("not permutation of each other");
+                }
 	}
 }
